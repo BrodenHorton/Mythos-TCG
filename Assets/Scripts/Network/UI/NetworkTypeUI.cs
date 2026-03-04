c2c249d08411e7dc1cab18a673894a76265e0005
SDRenegade <brody_horton@hotmail.com>
2026-03-04 06:48:23 -0500

    Added NetworkManager and created first build of the game.

---
+++ Assets/Scripts/Network/UI/NetworkTypeUI.cs
@@ -0,0 +1,19 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkTypeUI : MonoBehaviour {
+    [SerializeField] private Button host;
+    [SerializeField] private Button client;
+
+    private void Awake() {
+        host.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+            gameObject.SetActive(false);
+        });
+        client.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+            gameObject.SetActive(false);
+        });
+    }
+}
